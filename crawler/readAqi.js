dc9300988e9d55272bdf2a19c57a56a7167a48d9
litao <litao@waixie.com>
2017-01-17 17:21:36 +0800

    增加读取mongodb数据的脚本

---
+++ crawler/readAqi.js
@@ -0,0 +1,30 @@
+const conf       = require('../myapp/config/conf.json');
+const mongoose   = require('mongoose');
+mongoose.Promise = global.Promise;
+const co         = require('co');
+
+let readAqi = () => {
+	return new Promise( (resolve,reject) => {
+		mongoose.connect('mongodb://' + conf.user + ':' + conf.pwd + '@localhost/es6');
+		let db = mongoose.connection;
+
+		db.on('error', err => {
+			if(err) console.log( '链接数据库错误： ' + err );
+		});
+
+		db.on('open',() => {
+			let weatherSchema = mongoose.Schema();
+			let weatherModel = mongoose.model('bj_weather',weatherSchema);
+			weatherModel.find( (err,docs) => {
+				resolve(docs);
+			});
+		});
+	});
+}
+
+co(function* (){
+	let result = yield readAqi();
+	console.log(result);
+}).catch( err => {
+	if(err) console.log( 'in co catch:: ' + err );
+});
