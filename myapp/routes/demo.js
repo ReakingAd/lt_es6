2d37d96c73db5480664c772878da00fee24c6420
litao <litao@waixie.com>
2017-02-28 10:15:32 +0800

    修改fs路径的获取方式，

--- myapp/routes/demo.js
+++ myapp/routes/demo.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router  = express.Router();
 const ejs     = require('ejs');
 const fs      = require('fs');
+const path    = require('path'); 
 
-/* GET home page. */
 router.get('/pubsub/index', (req, res, next) => {
 	res.render('demo/pubsub/index');
 });
 
 router.get('/pubsub/getContent',(req,res) => {
-	let {tplName} = req.query;
-	let str       = fs.readFileSync('./views/demo/pubsub/' + tplName + '.ejs').toString();
-	let html      = ejs.render(str,{test: 'value123'});
-
+	let { tplName } = req.query;
+	let tplPath     = path.join( __dirname,'../views/demo/pubsub/' + tplName + '.ejs' ); 
+	let str         = fs.readFileSync( tplPath ).toString();
+	let html        = ejs.render( str,{test: 'value123'} );
 	res.send( html );
 });
 
